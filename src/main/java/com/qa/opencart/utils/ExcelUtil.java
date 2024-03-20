fbd859de9e6c9e6e95594dd07f17ebab53402ae8
Md Waseem Raza <waseem.razain@gmail.com>
2024-03-20 19:31:47 +0530

    adding excel util

--- src/main/java/com/qa/opencart/utils/ExcelUtil.java
+++ src/main/java/com/qa/opencart/utils/ExcelUtil.java
@@ -1,5 +1,45 @@
 package com.qa.opencart.utils;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
 public class ExcelUtil {
 
+	public static final String TEST_DATA_SHEET_PATH = "C:/Users/wasee/Downloads/OpenCartTestData.xlsx";
+	public static Workbook book;
+	public static Sheet sheet;
+
+	public void getTestData(String sheetName) {
+		System.out.println("Reading the data from sheet : " + sheetName);
+
+		Object data[][] = null;
+
+		try {
+			FileInputStream ip = new FileInputStream(TEST_DATA_SHEET_PATH);
+			try {
+				book = WorkbookFactory.create(ip);
+				sheet = book.getSheet(sheetName);
+
+				data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()];
+
+				for (int i = 0; i < sheet.getLastRowNum(); i++) {
+					for (int j = 0; j < sheet.getRow(0).getLastCellNum(); j++) {
+						data[i][j] = sheet.getRow(i + 1).getCell(j).toString();
+					}
+				}
+
+			} catch (EncryptedDocumentException | IOException e) {
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
 }
